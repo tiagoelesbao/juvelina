b137a61252c63397828e36effac6da1076dd13b8
tiagoelesbao <128316589+tiagoelesbao@users.noreply.github.com>
2025-05-09 17:28:54 -0300

    este

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,17 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  base: '/juvelina/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@features': path.resolve(__dirname, './src/features'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@context': path.resolve(__dirname, './src/context')
+      '@assets': path.resolve(__dirname, './src/assets')
     }
-  },
-  // Outras configurações...
+  }
 });
